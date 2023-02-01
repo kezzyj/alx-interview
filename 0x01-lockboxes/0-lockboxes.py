95671e18c137d2568b5b92af317e22287a55d292
kezzyj <karachidavid31@gmail.com>
2023-02-01 17:35:22 +0000

    attempting the lockboxes task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
 
 """ You have n number of locked boxes in front of you. Each box is numbered
-
 sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
-
 """
 
 
-
-
-
 def canUnlockAll(boxes):
-
     """ Write a method that determines if all the boxes can be opened.
-
     @boxes is a list of lists
-
     """
 
     keys = [0]
