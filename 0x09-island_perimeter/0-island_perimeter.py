85a6bf1c069c8044cfcdcb778846ba4d1407fa4a
kezzyj <karachidavid31@gmail.com>
2023-03-30 11:57:37 +0000

    fixing identation

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
+"""
+Module find the perimeter of
+and Island
+"""
+
+
 def island_perimeter(grid):
-	row = len(grid)
-	col = len(grid[0])
-	perimeter = 0
-		# Loop through row
-	for i in range(row):
-		# Loop through each column
-        for j in range(col):
-            # If the cell is land
-            if grid[i][j] == 1:
-                # Initialize a variable to count the water neighbors
-                water = 0
-                # Check the neighbor above
-                if i == 0 or grid[i-1][j] == 0:
-                    water += 1
-                # Check the neighbor below
-                if i == m-1 or grid[i+1][j] == 0:
-                    water += 1
-                # Check the neighbor to the left
-                if j == 0 or grid[i][j-1] == 0:
-                    water += 1
-                # Check the neighbor to the right
-                if j == n-1 or grid[i][j+1] == 0:
-                    water += 1
-                # Add the water count to the perimeter
-                perimeter += water
-    # Return the perimeter as the answer
+    """ return perimeter of island
+    """
+    perimeter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                if col  == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                if len(grid[row]) == col + 1 or grid[row][col + 1] == 0:
+                    perimeter += 1
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                if len(grid) == row+1 or grid[row + 1][col] == 0:
+                    perimeter += 1
     return perimeter
